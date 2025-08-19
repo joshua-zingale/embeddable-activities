730fb6c1eaeb8bb60b596150a6561422f4465ff4
AgentWombat <jtzingale@gmail.com>
2025-08-18 19:04:35 -0700

    Initial commit

---
+++ app.py
@@ -0,0 +1,96 @@
+import json
+import secrets
+from typing import Annotated, Type, TypeVar
+from fastapi import FastAPI, Depends, Response, Cookie, Request
+from pydantic import BaseModel, Field
+from cryptography.fernet import Fernet
+
+
+app = FastAPI()
+
+master_key = Fernet.generate_key()
+
+
+class EncryptedPayload(BaseModel):
+    """Fernet Encrypted Data"""
+
+    encrypted_data: bytes
+
+    @staticmethod
+    def from_plaintext(text: str):
+        return EncryptedPayload(
+            encrypted_data=Fernet(master_key).encrypt(text.encode("utf-8"))
+        )
+
+    _T = TypeVar("_T", bound=BaseModel)
+
+    def decrypt_as(self, model_type: Type[_T]) -> _T:
+        return model_type(
+            **json.loads(
+                Fernet(master_key).decrypt(self.encrypted_data).decode("utf-8")
+            )
+        )
+
+
+class Activity(BaseModel):
+    # Identifier should be a valid cookie name
+    identifier: str = Field(pattern=r"^[\w\d\-_]+$")
+    answers: list[str]
+    hints: dict[str, str] | None = None
+    activity_metadata: dict | None = None
+
+
+class ActivitySubmission(EncryptedPayload):
+    submission: str
+
+
+class SubmissionResponse(BaseModel):
+    correct: bool
+    hint: str | None
+
+
+def get_session_id(
+    response: Response, session_id: Annotated[str | None, Cookie()] = None
+) -> str:
+    if session_id is None:
+        session_id = secrets.token_urlsafe(16)
+        response.set_cookie(key="session_id", value=session_id)
+    return session_id
+
+
+@app.post("/encrypt-activity")
+def encrypt_activity(activity: Activity) -> EncryptedPayload:
+    """Encrypt an activity for embedding in a client."""
+    activity_json = activity.model_dump_json()
+    return EncryptedPayload.from_plaintext(activity_json)
+
+
+@app.get("/check-if-solved")
+def check_if_solved(request: Request, identifier: str) -> bool:
+    value = request.cookies.get(get_activity_cookie_name(identifier))
+    if value is None:
+        return False
+    return value.lower() == "true"
+
+
+@app.post("/check-submission")
+def check_submission(
+    response: Response, submission: ActivitySubmission
+) -> SubmissionResponse:
+    activity = submission.decrypt_as(Activity)
+
+    is_correct = submission.submission in activity.answers
+
+    if activity.hints:
+        hint_text = activity.hints.get(submission.submission)
+    else:
+        hint_text = None
+
+    if is_correct:
+        response.set_cookie(get_activity_cookie_name(activity.identifier), "true")
+
+    return SubmissionResponse(correct=is_correct, hint=hint_text)
+
+
+def get_activity_cookie_name(identifier: str):
+    return f"activity-{identifier}"
